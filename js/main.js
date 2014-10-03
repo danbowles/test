7234785242469942f3405b462dfbc009ff1a68e9
Dan Bowles <daniel.bowles@gmail.com>
2014-10-03 13:07:32 -0400

    Updated selection of cars after one is added, added MutationObserver for select box

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-(function($, cars) {
+(function($, cars, MutationObserver) {
   var availableCars = cars;
   var dreamCars = [];
   var $carsSelect = $('select');
@@ -23,27 +23,44 @@
 
   function refreshAvailableCarsSelect() {
     $carsSelect.find('option').remove();
-    availableCars.forEach(function(car, index) {
+    availableCars.forEach(function(car) {
       $carsSelect.append(
         $('<option>')
         .text(car.make + ' ' + car.model)
-        .val(index)
+        .val(car.make)
       );
     });
+  }
+
+  function updateInstructions() {
+    if ($carsList.children().length) {
+      $('.no-cars').addClass('hide');
+    } else {
+      $('.no-cars').removeClass('hide');
+    }
+  }
+
+  function initSelectBoxObserver() {
+    
   }
 
   function refreshCarList() {
     $carsList.find('li').remove();
-    dreamCars.forEach(function(car, index) {
+    dreamCars.forEach(function(car) {
       $carsList.append(
         $('<li>').text(car.make + ' ' + car.model)
       );
     });
+    $carsList.find('li').append(
+      $('<a href="#">').text('Remove')
+    );
   }
 
-  function addCarToList(event) {
+  function addCarToList() {
     var $selectedCar = $carsSelect.find(':selected');
     var selectedIndex = $selectedCar.index();
+    var selectedMake = $selectedCar.val();
+    var nextIndex;
 
     if (!$selectedCar.length) {
       return;
@@ -60,13 +77,22 @@
       .eq(selectedIndex)
       .remove();
 
+    nextIndex = $carsSelect
+      .find('option[value="' + selectedMake + '"]')
+      .first().index();
+
+    if (nextIndex < 0) {
+      nextIndex = selectedIndex;
+    }
+
     $carsSelect
-      .find('option').eq(selectedIndex)
+      .find('option').eq(nextIndex)
       .prop('selected', true);
   }
 
   function removeCarFromList(event) {
-    var $carToRemove = $carsList.find(event.target);
+    event.preventDefault();
+    var $carToRemove = $carsList.find($(event.target).parent());
     var carIndex = $carToRemove.index();
 
     availableCars.push(
@@ -77,20 +103,28 @@
     refreshAvailableCarsSelect();
 
     $carToRemove.remove();
-
   }
   
   function init() {
+    var observer;
+
     sortAvailableCars();
     refreshAvailableCarsSelect();
+    initSelectBoxObserver();
 
     // Events
     $addToListButton.on('click', addCarToList);
-    $carsList.on('click', 'li', removeCarFromList);
+    $carsList.on('click', 'li a', removeCarFromList);
+
+    // Select Observer
+    observer = new MutationObserver(updateInstructions);
+    observer.observe($carsSelect[0], {
+      childList: true
+    });
   }
 
   $(function() {
     init();
   });
 
-})(jQuery, dreamCars);
+})(jQuery, dreamCars, (window.MutationObserver || window.WebKitMutationObserver));
